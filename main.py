69419e870718a17193496dbcc97838cd9169ad54
Tolib-prog <abutolibibodulloev@gmail.com>
2023-02-02 21:06:58 +0300

    learn -0.0.1

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# distance = int(input("Растояние в км:"))
-# speed_car = int(input("Планирумая среднаяя скорость в :"))
-# time = distance * 60 // speed_car
-# print(distance, "километров:"  " ", "Вы проеедете за", time, "минут!")
-buy_car = input("what do you need a car:")
-name_car = input("Name the car model:")
-model_car = input("Wich to buy the brand a car?:")
-cost_car = int(input("How much to spend money that to buy a car?:"))
-service = int(input("maintenance cost,'Стоимость ТО':"))
-fuel = int(input("Cost for fuel:"))
-tax = int(input("What the price for tax on the car?:"))
-insurance = int(input("what the price for insurance?:"))
-total = cost_car + service + fuel + tax + insurance
+# форматирования строки с помощью метода format()
+print('{0} {1} cost per three ${2} {3} {4} {5}'.format(3, "apples", 4, "dollars", 50, "cents"))
+print('{} {} cost ${}'.format(6, "bananas", 2.48))
+# f строки и работа с ними
+apple = 7
+greypfrut = 6
+watermilow = 2
+print(f'{apple} please eating {greypfrut} and {watermilow} today')
 
-print("For All:", total)
+num = input("Enter a number to be doubled:")
+doubled_num = int(num) * 12
+print(doubled_num, 'you find your number!')
